42af357e20a2668ebc2cfd29a2be7b28482a996a
Nilton Morais <nilton_even@hotmail.com>
2015-05-05 01:41:54 -0300

    Criação da base do módulo Aluno

---
+++ module/Aluno/config/module.config.php
@@ -0,0 +1,51 @@
+<?php
+ 
+return array(
+    # definir e gerenciar controllers
+    'controllers' => array(
+        'invokables' => array(
+            'HomeController' => 'Aluno\Controller\HomeController'
+        ),
+    ),
+ 
+    # definir e gerenciar rotas
+    'router' => array(
+        'routes' => array(
+            'home' => array(
+                'type'      => 'Literal',
+                'options'   => array(
+                    'route'    => '/',
+                    'defaults' => array(
+                        'controller' => 'HomeController',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+ 
+    # definir e gerenciar servicos
+    'service_manager' => array(
+        'factories' => array(
+            #'translator' => 'ZendI18nTranslatorTranslatorServiceFactory',
+        ),
+    ),
+ 
+    # definir e gerenciar layouts, erros, exceptions, doctype base
+    'view_manager' => array(
+        'display_not_found_reason'  => true,
+        'display_exceptions'        => true,
+        'doctype'                   => 'HTML5',
+        'not_found_template'        => 'error/404',
+        'exception_template'        => 'error/index',
+        'template_map'              => array(
+            'layout/layout'         => __DIR__ . '/../view/layout/layout.phtml',
+            'aluno/home/index'    => __DIR__ . '/../view/aluno/home/index.phtml',
+            'error/404'             => __DIR__ . '/../view/error/404.phtml',
+            'error/index'           => __DIR__ . '/../view/error/index.phtml',
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+);
